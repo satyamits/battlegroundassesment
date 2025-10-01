f7f78b2c10b8591fb1344547fc344576fb27c244
Satyam Singh <duggu1705@gmail.com>
2025-10-01 20:16:41 +0530

    Initial Commit

---
+++ BattleGroundAssement/BattleGroundAssementApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleGroundAssementApp.swift
+//  BattleGroundAssement
+//
+//  Created by Satyam Singh on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BattleGroundAssementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BattleGroundAssementTests/BattleGroundAssementTests.swift
@@ -0,0 +1,17 @@
+//
+//  BattleGroundAssementTests.swift
+//  BattleGroundAssementTests
+//
+//  Created by Satyam Singh on 01/10/25.
+//
+
+import Testing
+@testable import BattleGroundAssement
+
+struct BattleGroundAssementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
